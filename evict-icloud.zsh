3c7082f78a731afb62bd9adb633ab2e6fc1bbf85
willjasen <brandon@willjasen.com>
2026-03-18 10:20:52 -0400

    handle directories

--- evict-icloud.zsh
+++ evict-icloud.zsh
@@ -1,1 +1,11 @@
-find . -type f -exec sh -c 'xattr -p "com.apple.fileprovider.pinned#PX" "$1" >/dev/null 2>&1 && (echo "Processing: $1"; xattr -d "com.apple.fileprovider.pinned#PX" "$1"; brctl evict "$1")' -- {} \;
+#!/bin/zsh
+
+find . \( -type f -o -type d \) -exec sh -c '
+  xattr -p "com.apple.fileprovider.pinned#PX" "$1" >/dev/null 2>&1
+  if [ $? -eq 0 ]; then
+    REL="${1#./}"
+    echo "Processing: $REL"
+    xattr -d "com.apple.fileprovider.pinned#PX" "$1"
+    brctl evict "$1"
+  fi
+' -- {} \;
