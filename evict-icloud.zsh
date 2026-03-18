021f66054515a562b510762ac2993d89e81c78d1
willjasen <brandon@willjasen.com>
2026-03-18 10:31:11 -0400

    display message if nothing was evicted

--- evict-icloud.zsh
+++ evict-icloud.zsh
@@ -1,5 +1,8 @@
 #!/bin/zsh
 
+# Track whether any items were evicted
+evicted=0
+
 # This function checks if the item is pinned and evicts it if it is.
 check_item_to_evict() {
   local item="$1"
@@ -9,9 +12,14 @@ check_item_to_evict() {
     echo "Processing: $rel"
     xattr -d "com.apple.fileprovider.pinned#PX" "$item"
     brctl evict "$item"
+    evicted=1
   fi
 }
 
 find . \( -type f -o -type d \) -print0 | while IFS= read -r -d '' item; do
   check_item_to_evict "$item"
 done
+
+if [ "$evicted" -eq 0 ]; then
+  echo "No files or directories found to evict."
+fi
